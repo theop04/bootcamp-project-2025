f4918ec232999367109923beed3366b30feb9e01
Theodore Weicker <tweickerv@gmail.com>
2025-11-21 23:59:51 -0800

    feat: refactored files to be more consistent and added comment api skeleton

---
+++ bootcamp-milestone-4/src/app/api/blog/[slug]/comment/route.ts
@@ -0,0 +1,13 @@
+
+
+// export async function POST(req: NextRequest) {
+// 	const body = req.body
+// 	const BlogSlug = the blog slug from the request params
+// 	// validate body
+// 	if (body not valid) {
+// 		return some error
+// 	}
+	
+// 	// push comment object to document
+// 	Blog.update(...)
+// }
